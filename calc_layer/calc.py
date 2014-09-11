1520055bf73c4f11034c322da13ca9260a1230cd
Jialei <jialeicui@126.com>
2014-09-11 23:01:26 +0800

    Update calc.py

--- calc_layer/calc.py
+++ calc_layer/calc.py
@@ -2,7 +2,7 @@
 
 import xlrd
 
-w = xlrd.open_workbook(u'2.xlsm')
+w = xlrd.open_workbook(u'3.xlsm')
 data_sheet = w.sheet_by_name(u'data_table')
 
 def get_column_names(xl_sheet, name):
@@ -18,6 +18,9 @@ tnb_col = get_column_names(data_sheet, 'ques_7_2') # 0/1
 smoke_col = get_column_names(data_sheet, 'ques_9_1') # 0/1
 danguchun_col = get_column_names(data_sheet, 'ques_10_2')
 sbp_col = get_column_names(data_sheet, 'ques_10_1_1')
+layer_col = get_column_names(data_sheet, 'ques_10_16')
+jiangya_col = get_column_names(data_sheet, 'ques_7_6_1')
+jiangzhi_col = get_column_names(data_sheet, 'ques_7_6_2')
 
 layer = {'1':{}, '2':{}, '3':{}, '4':{}}
 #gender age tnb smoke
@@ -30,10 +33,40 @@ layer['2']['5'] = [[3,4,4,4,4,0,1,1,2,4,0,0,0,1,2,0,0,0,0,0,],[4,4,4,4,4,1,2,3,4
 layer['2']['6'] = [[4,4,4,4,4,2,2,3,4,4,1,1,1,2,3,0,0,0,1,1,],[4,4,4,4,4,2,3,4,4,4,1,1,2,2,4,0,0,1,1,2,],[2,2,3,4,4,0,0,1,1,2,0,0,0,0,1,0,0,0,0,0,],[3,4,4,4,4,1,1,2,2,4,0,0,1,1,2,0,0,0,0,1,],]
 layer['2']['7'] = [[4,4,4,4,4,2,2,3,4,4,1,1,2,2,3,0,0,1,1,1,],[4,4,4,4,4,3,4,4,4,4,2,2,3,4,4,1,1,2,2,2,],[2,3,3,4,4,1,1,1,1,2,0,0,0,1,1,0,0,0,0,0,],[3,4,4,4,4,1,2,2,3,4,1,1,1,1,2,0,0,0,1,1,],]
 
-def get_layer(d):
+def get_danguchun(d):
+    for x in xrange(5,9):
+        if d < x:
+            return x - 5
+        pass
+    return 4
+    pass
+
+def get_sbp(d):
+    if d <= 139:
+        return 0
+    elif d <= 159:
+        return 1
+    elif d <= 179:
+        return 2
+    else:
+        return 3
+    pass
+
+def get_in_group(group, d):
+    dgc = get_danguchun(d['danguchun'])
+    sbp = get_sbp(d['sbp'])
+    # print dgc, sbp
+    return group[(3 - sbp) * 5 + dgc]
+    pass
+
+def get_layer(d, row):
     age = str((int(d['age']))/10)
+    if int(age) > 7:
+        age = '7'
+    if int(age) < 4:
+        age = '4'
     group = layer[str(int(d['gender']))][age]
-    print d
+    # print d
     if d['tnb']:
         if not d['smoke']:
             g = group[0]
@@ -45,10 +78,11 @@ def get_layer(d):
         else:
             g = group[3]
     pass
-    
-    print g
+    # print g
+    return get_in_group(g, d)
 
 def get(row):
+    # data = {'gender':2,'age':48, 'smoke':0, 'tnb':0, 'danguchun':7, 'sbp':181}
     data = {}
     data['gender'] = data_sheet.cell_value(row, gender_col)
     data['age'] = data_sheet.cell_value(row, age_col)
@@ -60,11 +94,28 @@ def get(row):
     for k,v in data.items():
         if v == u'missing':
             return None
+    if data['gender'] == '' or data['age'] == '':
+            return None
+    layer = get_layer(data, row)
+    jy = data_sheet.cell_value(row, jiangya_col)
+    jz = data_sheet.cell_value(row, jiangzhi_col)
+    if jy or jz:
+        layer = layer + 1
+        if layer > 4:
+            layer = 4
 
-    return get_layer(data)
+    return layer
     pass
 
+
 if __name__ == '__main__':
-    for x in xrange(1,10):
-        get(x)
+    f = open('list.txt', 'w')
+    for x in xrange(1,102054):
+        res = get(x)
+        if res != None:
+            res = res + 1
+        else:
+            res = ''
+        f.write(str(res) + '\n')
         pass
+    f.close()
